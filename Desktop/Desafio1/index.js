76348ed125298feb2068b00a1a03871ac60e5482
oscaralfa <avioika@hotmail.com>
2023-05-25 11:34:17 -0600

    Creando archivo index.html con desafio1

---
+++ Desktop/Desafio1/index.js
@@ -0,0 +1,68 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = ".encriptado.jpg";
+    } else {
+      muñeco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      alert("Debes ingresar algún texto");
+    }
+  }
+  
+  function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+  
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/desencriptado.jpg";
+      } else {
+        muñeco.src = "./img/encriptado.jpg";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+      }
+
+      function copy() {
+        let copyText = document.querySelector("#input");
+        copyText.select();
+        document.execCommand("copy");
+      }
+
+      document.querySelector("#copy").addEventListener("click", copy);
+
+      function paste() {
+        let pasteText = document.querySelector("#output");
+        pasteText.focus();
+        document.execCommand("paste");
+        console.log(pasteText.textContent);
+      }
+      
+      document.querySelector("#paste").addEventListener("click", paste);
+  }
